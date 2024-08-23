d5680c2b18e0d0b9869a22aedb1c6d80100e410d
ArnavMeh <mehraarn000@gmail.com>
2024-08-22 23:49:39 -0400

    id-based link

--- src/test/scala/lms/tutorial/idk.scala
+++ src/test/scala/lms/tutorial/idk.scala
@@ -3,44 +3,59 @@ package scala.lms.tutorial.idk
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.HashSet
 import scala.collection.mutable.ArrayBuffer
+import java.util.Base64
 
-class Data
-
-object Data {
+object JS {
+    class Data
     case class Number(value: Double = 0) extends Data
     case class Str   (value: String = "") extends Data
-    case class Array (value: ArrayBuffer[Data] = ArrayBuffer()) extends Data
-    case class Object(value: HashMap[Data, Data] = HashMap()) extends Data
+    case class Array (value: ArrayBuffer[JS.Data] = ArrayBuffer()) extends Data
+    case class Object(value: HashMap[JS.Data, JS.Data] = HashMap()) extends Data
     case class Bool  (value: Boolean = false) extends Data
-    case class Func  (value: Function[Data.Array, Data] = (_ => Data.Undef())) extends Data
+    case class Func  (value: Function[JS.Array, JS.Data] = (_ => JS.Undef())) extends Data
     case class Null  () extends Data
     case class Undef () extends Data
 
     def createElement(
-        tag: Data.Str,
-        attrs: Data.Object,
-        children: Data.Array
-    ): Data.Object = {
-        val m: HashMap[Data, Data] = HashMap(
-            Data.Str("tag") -> tag,
-            Data.Str("children") -> children,
-            Data.Str("replaceChildren") -> Data.Func((children: Data.Array) => {
+        tag: JS.Str,
+        attrs: JS.Object,
+        children: JS.Array
+    ): JS.Object = {
+        val m: HashMap[JS.Data, JS.Data] = HashMap(
+            JS.Str("tag") -> tag,
+            JS.Str("children") -> children,
+            JS.Str("replaceChildren") -> JS.Func((children: JS.Array) => {
                 children.value.clear()
                 children.value.appendAll(children.value)
-                Data.Undef()
+                JS.Undef()
             })
         )
-        Data.Object(m)
+        JS.Object(m)
     }
 
-    def createText(value: Data.Str): Data.Object = {
-        val m: HashMap[Data, Data] = HashMap(Data.Str("textContent") -> value)
-        Data.Object(m)
+    def createText(value: JS.Str): JS.Object = {
+        val m: HashMap[JS.Data, JS.Data] = HashMap(JS.Str("textContent") -> value)
+        JS.Object(m)
+    }
+
+    def findElementById(value: JS.Str): JS.Object = {
+        JS.Object()
+    }
+}
+
+object Enumerator {
+    val encoder = Base64.getEncoder()
+    var next_val: BigInt = 0
+
+    def nextId() = {
+        val bytes: Array[Byte] = next_val.toByteArray
+        next_val += 1
+        encoder.encodeToString(bytes)
     }
 }
 
 trait Valued {
-    var value: Data = Data.Undef()
+    var value: JS.Data = JS.Undef()
 }
 
 trait Effectful {
@@ -49,17 +64,16 @@ trait Effectful {
     def remEffect = effects.remove(_)
 }
 
-abstract class VE extends Effectful with Valued 
+trait VE extends Effectful with Valued 
 
 object Effect {
-    type Callback = Function[ArrayBuffer[Data], Data]
+    type Callback = Function[ArrayBuffer[JS.Data], JS.Data]
 }
 
 class Effect(
     callback: Effect.Callback,
     parents: ArrayBuffer[VE]
-)
-extends VE {
+) extends VE {
     parents.foreach(_.addEffect(this))
     value = {
         val args = parents.map(_.value)
@@ -77,11 +91,10 @@ extends VE {
     }
 }
 
-class State(val v: Data)
-extends VE() {
+class State(val v: JS.Data) extends VE {
     value = v
 
-    def set(x: Either[Data, Function[Data, Data]]) = {
+    def set(x: Either[JS.Data, Function[JS.Data, JS.Data]]) = {
         value = x match {
             case Left(new_value) => new_value
             case Right(transform) => transform(value)
@@ -91,13 +104,19 @@ extends VE() {
 }
 
 abstract class Component(
-    val wrapper: Data.Object,
-    val props: HashMap[Data, Data],
+    val wrapper_tag: String,
+    val wrapper_attrs: Either[HashMap[String, Either[JS.Data, VE]], VE] = Left(HashMap()),
+    val props: HashMap[JS.Data, JS.Data] = HashMap(),
     val states: ArrayBuffer[State] = ArrayBuffer(),
     val effects: ArrayBuffer[Effect] = ArrayBuffer(),
     val children: ArrayBuffer[Component] = ArrayBuffer()
 ) {
-    def createState(value: Data) = {
+    val (wrapper_id, wrapper_str) = element(
+        wrapper_tag, wrapper_attrs,
+        ArrayBuffer(render())
+    )
+
+    def createState(value: JS.Data) = {
         val st = new State(value)
         states.append(st)
         st
@@ -113,9 +132,9 @@ abstract class Component(
     }
 
     def mount(): Unit = {
-        val root = render()
-        val fn = wrapper.value(Data.Str("replaceChildren")).asInstanceOf[Data.Func]
-        fn.value(Data.Array(ArrayBuffer(root)))
+        val (root_id, root_str) = render()
+        val wrapper_el = JS.findElementById(JS.Str(wrapper_id))
+        wrapper_el.value(JS.Str("innerHTML")) = JS.Str(root_str)
 
         onMount()
         children.foreach(_.mount())
@@ -128,117 +147,132 @@ abstract class Component(
         effects.foreach(_.cleanup())
         onCleanup()
 
-        val fn = wrapper.value(Data.Str("replaceChildren")).asInstanceOf[Data.Func]
-        fn.value(Data.Array())
+        val wrapper_el = JS.findElementById(JS.Str(wrapper_id))
+        val fn = wrapper_el.value(JS.Str("replaceChildren")).asInstanceOf[JS.Func]
+        fn.value(JS.Array())
     }
 
     def onCleanup() = {}
 
-    def render(): Data.Object
+    def render(): (String, String)
 
     def component[NewComponent <: Component](comp: NewComponent) = {
         children.append(comp)
-        wrapper
+        (comp.wrapper_id, comp.wrapper_str)
     }
 
-    def text(value: Either[String, VE]) = {
-        val node = Data.createText(Data.Str(""))
-        value match {
-            case Left(str) => {
-                node.value(Data.Str("textContent")) = Data.Str(str)
-            }
-            case Right(ef) => {
-                createEffect(
-                    (params) => {
-                        val new_str = params(0).asInstanceOf[Data.Str]
-                        node.value(Data.Str("textContent")) = new_str
-                        new_str
-                    },
-                    ArrayBuffer(ef)
-                )
-            }
-        }
-        node
-    }
+    // def text(value: Either[String, VE]) = {
+    //     val id = Enumerator.nextId()
+    //     val text_val = value match {
+    //         case Left(dt) => dt.toString()
+    //         case Right(ef) => {
+    //             val new_ef = createEffect(
+    //                 (params) => {
+    //                     val new_str = params(0).asInstanceOf[JS.Str]
+    //                     val el = JS.findElementById(JS.Str(id))
+    //                     el.value(JS.Str("textContent")) = new_str
+    //                     new_str
+    //                 },
+    //                 ArrayBuffer(ef)
+    //             )
+    //             new_ef.value.toString()
+    //         }
+    //     }
+
+    //     (id, text_val)
+    // }
 
     def element(
         tag: String,
-        attrs: Either[HashMap[String, Either[Data, VE]], VE] = Left(HashMap()),
-        children: ArrayBuffer[Data] = ArrayBuffer()
+        attrs: Either[HashMap[String, Either[JS.Data, VE]], VE] = Left(HashMap()),
+        children: ArrayBuffer[(String, String)] = ArrayBuffer()
     ) = {
-        val node: Data.Object = Data.createElement(
-            Data.Str(tag),
-            Data.Object(HashMap()),
-            Data.Array(children)
-        )
-        attrs match {
+        val id = Enumerator.nextId()
+        val attr_map = attrs match {
             case Left(map) => {
-                map.foreach(p => {
-                    val key = Data.Str(p._1)
+                map.map(p => {
+                    val key = JS.Str(p._1)
                     val value = p._2
                     value match {
                         case Left(dt) => {
-                            node.value(key) = dt
+                            (key, dt)
                         }
                         case Right(ef) => {
-                            createEffect(
+                            val new_ef = createEffect(
                                 (params) => {
-                                    val new_dt = params(0).asInstanceOf[Data.Str]
-                                    node.value(key) = new_dt
+                                    val new_dt = params(0).asInstanceOf[JS.Str]
+                                    val el = JS.findElementById(JS.Str(id))
+                                    el.value(key) = new_dt
                                     new_dt
                                 },
                                 ArrayBuffer(ef)
                             )
+                            (key, new_ef.value)
                         }
                     }
                 })
             }
             case Right(ef) => {
-                createEffect(
+                val new_ef = createEffect(
                     (params) => {
-                        val prev_val = ef.value.asInstanceOf[Data.Object]
-                        val new_val = params(0).asInstanceOf[Data.Object]
-                        prev_val.value.keys.foreach(node.value.remove(_))
-                        new_val.value.foreach(p => node.value(p._1) = p._2)
+                        val prev_val = ef.value.asInstanceOf[JS.Object]
+                        val new_val = params(0).asInstanceOf[JS.Object]
+
+                        val el = JS.findElementById(JS.Str(id))
+                        prev_val.value.keys.foreach(el.value.remove(_))
+                        new_val.value.foreach(p => el.value(p._1) = p._2)
                         new_val
                     },
                     ArrayBuffer(ef)
                 )
+                new_ef.value.asInstanceOf[JS.Object].value
             }
         }
-        node
+        val attrs_str = attr_map.foldLeft("")((acc, x) => acc + " " + x._1 + "=\"" + x._2 + "\"")
+        val children_str = children.map(_._2).foldLeft("")((acc, x) => acc + "\n" + x)
+
+        (
+            id, 
+            "<" + tag
+                + " id=\"" + id + "\""
+                + attrs_str
+            + ">"
+                + children_str
+            + "\n</" + tag + ">"
+        )
     }
 }
 
-class ExampleParent(
-    wrapper: Data.Object,
-    props: HashMap[Data, Data] = HashMap()
-) extends Component(wrapper, props) {
+class ExampleParent extends Component("div") {
     def render() = {
         element("div", Left(HashMap()), ArrayBuffer(
-            component(new ExampleChild(element("div")))
+            component(new ExampleChild())
         ))
     }
 }
 
 class ExampleChild(
-    wrapper: Data.Object,
-    props: HashMap[Data, Data] = HashMap()
-) extends Component(wrapper, props) {
+    props: HashMap[JS.Data, JS.Data] = HashMap()
+) extends Component("div", Left(HashMap()), props) {
+    val s = createState(JS.Number(0))
     def render() = {
-        text(Left("child"))
+        element("div", Left(HashMap(
+            "style" -> Left(JS.Str("color: red"))
+        )), ArrayBuffer(
+            element("div", Left(HashMap(
+                "textContent" -> Left(JS.Str("counter: "))
+            ))),
+            element("div", Left(HashMap(
+                "textContent" -> Right(s)
+            )))
+        ))
     }
 }
 
 object Main {
     def main(args: Array[String]) = {
-        val root = Data.createElement(
-            new Data.Str("div"),
-            new Data.Object(),
-            new Data.Array()
-        )
-        val c = new ExampleParent(root)
-        print(c.render())
+        val c = new ExampleParent()
+        print(c.render()._2)
     }
 }
 
